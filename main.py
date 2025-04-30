d06307eb44450a2807ecc033f721ddf331f4e364
kaiser-lai <klai2027@kehillahstudent.org>
2025-04-30 12:51:39 -0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import json
 
 # read `expenses.json`
-
+filename = "expenses.json"
+with open (filename, "r") as f:
+    expense = json.load(f)
+    
 # get and print total "price" for all expenses at the "pet store"
+
+total = sum(item["price"] for item in expense["pet store"])
+
+print(total)
+    
